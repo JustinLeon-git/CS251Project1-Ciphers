42716751a06059481ea6a4d6fb001defc17ff0d5
Scritchy <justinjleon@gmail.com>
2025-02-02 23:22:27 -0600

    Done with most of the caesar cypher

---
+++ ciphers.cpp
@@ -0,0 +1,294 @@
+#include <cctype>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "include/caesar_dec.h"
+#include "include/caesar_enc.h"
+#include "include/subst_dec.h"
+#include "include/subst_enc.h"
+#include "utils.h"
+
+using namespace std;
+
+// Initialize random number generator in .cpp file for ODR reasons
+std::mt19937 Random::rng;
+
+const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// Function declarations go at the top of the file so we can call them
+// anywhere in our program, such as in main or in other functions.
+// Most other function declarations are in the included header
+// files.
+
+// When you add a new helper function, make sure to declare it up here!
+
+/**
+ * Print instructions for using the program.
+ */
+void printMenu();
+
+int main() {
+  Random::seed(time(NULL));
+  string command;
+  vector<string> dictionary;
+
+  // Reading in our dictionary for decryption later
+  ifstream dictFile("dictionary.txt");
+  string word;
+  while (getline(dictFile, word)) {
+    dictionary.push_back(word);
+    cout << word << endl;
+  }
+
+
+  cout << "Welcome to Ciphers!" << endl;
+  cout << "-------------------" << endl;
+  cout << endl;
+
+  // Command loop
+  do {
+    // Commands for user input
+    printMenu();
+    cout << endl << "Enter a command (case does not matter): ";
+
+    // Use getline for all user input to avoid needing to handle
+    // input buffer issues relating to using both >> and getline
+    getline(cin, command);
+    cout << endl;
+
+    // Seeding random generator
+    if (command == "R" || command == "r") {
+      string seed_str;
+      cout << "Enter a non-negative integer to seed the random number "
+              "generator: ";
+      getline(cin, seed_str);
+      Random::seed(stoi(seed_str));
+
+    // Encrypting
+    } else if (command == "C" || command == "c") {
+      caesarEncryptCommand();
+
+    // Decrypting
+    } else if (command == "D" || command == "d") {
+      caesarDecryptCommand(dictionary);
+    }
+
+    cout << endl;
+
+  } while (!(command == "x" || command == "X") && !cin.eof());
+
+  return 0;
+}
+
+void printMenu() {
+  cout << "Ciphers Menu" << endl;
+  cout << "------------" << endl;
+  cout << "C - Encrypt with Caesar Cipher" << endl;
+  cout << "D - Decrypt Caesar Cipher" << endl;
+  cout << "E - Compute English-ness Score" << endl;
+  cout << "A - Apply Random Substitution Cipher" << endl;
+  cout << "S - Decrypt Substitution Cipher from Console" << endl;
+  cout << "F - Decrypt Substitution Cipher from File" << endl;
+  cout << "R - Set Random Seed for Testing" << endl;
+  cout << "X - Exit Program" << endl;
+}
+
+// "#pragma region" and "#pragma endregion" group related functions in this file
+// to tell VSCode that these are "foldable". You might have noticed the little
+// down arrow next to functions or loops, and that you can click it to collapse
+// those bodies. This lets us do the same thing for arbitrary chunks!
+#pragma region CaesarEnc
+
+char rot(char c, int amount) {
+  // Find the location of the char in the alphabet
+  int charLocation = ALPHABET.find(c);
+
+  // Finds the new character after rotation and accounts for wrapping if too large a jump
+  c = ALPHABET.at((charLocation + amount) % 26);
+  return c;
+}
+
+string rot(const string& line, int amount) {
+  // Initializing our rotated string
+  string encrypted;
+
+  // Looping through every letter in our string
+  for (char c : line) {
+    // Checking if our character is a letter
+    if (isalpha(c)) {
+      // Running it through our char encrypter and adding it to encrypted string
+      encrypted.push_back(rot((char)toupper(c), amount));
+
+    // Otherwise we check if it is a space and add to our encrypted string
+    } else if (isspace(c)) {
+      encrypted += c;
+    }
+  }
+  return encrypted;
+}
+
+void caesarEncryptCommand() {
+  string inputText, rotation;
+
+  // Getting input from user
+  cout << "Text to encrypt: ";
+  getline(cin, inputText);
+  cout << "Amount to rotate: ";
+  getline(cin, rotation);
+
+  // Running encryption commands and printing output
+  cout << rot(inputText, stoi(rotation)) << endl;
+}
+
+#pragma endregion CaesarEnc
+
+#pragma region CaesarDec
+
+void rot(vector<string>& strings, int amount) {
+  // Looping through every word in our string vector
+  for (string& word : strings) {
+    word = rot(word, amount);
+  }
+}
+
+string clean(const string& s) {
+  string cleanString;
+  for (char c : s) {
+    // Checks if a char is a letter
+    if (isalpha(c)) {
+      cleanString.push_back(toupper(c));
+    }
+  }
+  return cleanString;
+}
+
+vector<string> splitBySpaces(const string& s) {
+  vector<string> splits;
+  string cleanedStr;
+  int lastIndex = 0;
+  int newIndex = 0;
+  // If our string contains nothing
+  if (s.empty()) {
+    return splits;
+  
+  // If our string has a single word
+  } else if (s.find(' ') == string::npos) {
+    splits.push_back(clean(s));
+    return splits;
+  }
+
+  // Loops over every word until we reach the end of the string
+  while (s.find(' ', lastIndex) != string::npos) {
+    newIndex = s.find(' ', lastIndex);
+    cleanedStr = clean(s.substr(lastIndex, newIndex - lastIndex));
+    // Check if our cleaned string has a word
+    if (!cleanedStr.empty()) {
+      splits.push_back(cleanedStr);
+    }
+    lastIndex = newIndex + 1;
+  }
+  // Checks the last of the string
+  cleanedStr = clean(s.substr(lastIndex, s.size() - lastIndex));
+  if (!cleanedStr.empty()) {
+    splits.push_back(cleanedStr);
+  }
+  return splits;
+}
+
+string joinWithSpaces(const vector<string>& words) {
+  // Adding in our first word
+  string joint = words.at(0);
+
+  // Looping through every word and adding a space before it and the next word
+  for (size_t i = 1; i < words.size(); i++) {
+    joint + ' ' + words.at(i);
+  }
+  cout << "JOINT: " << joint << endl;
+  return joint;
+}
+
+int numWordsIn(const vector<string>& words, const vector<string>& dict) {
+  int count = 0;
+  // Linear search of every word checking if it's in the dictionary
+  for (string word : words) {
+    for (string dictWord : dict) {
+      if (word == dictWord) {
+        count++;
+        break;
+      }
+    }
+  }
+  return count;
+}
+
+void caesarDecryptCommand(const vector<string>& dict) {
+  string encrypted;
+  vector<string> encryptedList, decryptAttempt;
+  int count;
+  bool decryptFlag = false;
+  cout << "Enter encrypted text: ";
+  getline(cin, encrypted);
+  // Cleaning up the encrypted text
+  encryptedList = splitBySpaces(encrypted);
+  
+  // Looping through every possible rotation
+  for (int i = 0; i < 26; i++) {
+    decryptAttempt = encryptedList;
+    // Rotate our decryption
+    rot(decryptAttempt, i);
+    // Check against dictionary
+    count = numWordsIn(decryptAttempt, dict);
+
+    // Valid decryptions printed
+    if (count > decryptAttempt.size()) {
+      decryptFlag = true;
+      cout << joinWithSpaces(decryptAttempt) << endl;
+    }
+  }
+  // Checking if we found any decryptions
+  if (!decryptFlag) {
+    cout << "No good decryptions found" << endl;
+  }
+}
+
+#pragma endregion CaesarDec
+
+#pragma region SubstEnc
+
+string applySubstCipher(const vector<char>& cipher, const string& s) {
+  // TODO: student
+  return "";
+}
+
+void applyRandSubstCipherCommand() {
+  // TODO: student
+}
+
+#pragma endregion SubstEnc
+
+#pragma region SubstDec
+
+double scoreString(const QuadgramScorer& scorer, const string& s) {
+  // TODO: student
+  return 0.0;
+}
+
+void computeEnglishnessCommand(const QuadgramScorer& scorer) {
+  // TODO: student
+}
+
+vector<char> decryptSubstCipher(const QuadgramScorer& scorer,
+                                const string& ciphertext) {
+  // TODO: student
+  return vector<char>{};
+}
+
+void decryptSubstCipherCommand(const QuadgramScorer& scorer) {
+  // TODO: student
+}
+
+#pragma endregion SubstDec
